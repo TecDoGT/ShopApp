7402a4430c92576b48846f329048f0ab24467388
TecDoGT <ddelaguila@tecdogt.com>
2016-04-27 10:05:05 -0600

    test Camara

--- www/js/camara.js
+++ www/js/camara.js
@@ -3,24 +3,26 @@
     try {
         var filename = "fotoTest1.jpg";
         
-        navigator.camera.getPicture(onSuccess, onFail, {
-            quality: 50,
-            destinationType: Camera.DestinationType.DATA_URL,
-            sourceType: Camera.PictureSourceType.CAMERA,
-            allowEdit: true,
-            encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 100,
-            targetHeight: 100,
-            correctOrientation: true,
-            saveToPhotoAlbum: true
-        });
+        navigator.camera.getPicture(onSuccess, onFail,
+            {
+                quality: 50,
+                destinationType: Camera.DestinationType.FILE_URI,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                correctOrientation: true,
+                saveToPhotoAlbum: true
+            });
 
-        function onSuccess(imageData) {
+        function onSuccess(imageData)
+        {
             var image = document.getElementById('tempFoto');
-            image.src = "data:image/jpeg;base64," + imageData;
+            alert(image);
+            image.src =  imageData;
         }
 
-        function onFail(message) {
+        function onFail(message)
+        {
             alert('Failed because: ' + message);
         }
     }
